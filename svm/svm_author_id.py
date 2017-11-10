5cf2c5c3f2760ecc464c710196e0b4adce6d8ea3
Nika Dogonadze <ndogh13@freeuni.edu.ge>
2017-11-11 00:21:54 +0400

    Complete SVM project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,21 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+clf = svm.SVC(C=10000, kernel='rbf')
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+predictions = clf.predict(features_test)
+print accuracy_score(predictions, labels_test)
+print "testing time:", round(time()-t0, 3), "s"
+print "Chris: ", sum(predictions)
 #########################################################
 
 
