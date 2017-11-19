288fe20e3e5359728465e923e63590b1db979353
Nika Dogonadze <ndogh13@freeuni.edu.ge>
2017-11-19 16:53:42 +0400

    Answer enron data exploring questions

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# coding=utf-8
 
 """ 
     Starter code for exploring the Enron dataset (emails + finances);
@@ -19,4 +20,33 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+# How many data points (people) are in the dataset?
+print len(enron_data)
 
+# How many data points (people) are in the dataset?
+print len(enron_data.values()[0])
+
+# count the number of entries in the dictionary where data[person_name]["poi"]==1
+print len([x for x in enron_data.values() if x['poi'] == 1])
+
+# What is the total value of the stock belonging to James Prentice?
+print enron_data['PRENTICE JAMES']['total_stock_value']
+
+# How many email messages do we have from Wesley Colwell to persons of interest?
+print enron_data['COLWELL WESLEY']['from_this_person_to_poi']
+
+# What’s the value of stock options exercised by Jeffrey K Skilling?
+print enron_data['SKILLING JEFFREY K']['exercised_stock_options']
+
+# Of these three individuals (Lay, Skilling and Fastow), who took
+# home the most money (largest value of “total_payments” feature)?
+print max(
+    enron_data['LAY KENNETH L']['total_payments'],
+    enron_data['SKILLING JEFFREY K']['total_payments'],
+    enron_data['FASTOW ANDREW S']['total_payments']
+)
+
+# How many folks in this dataset have a quantified salary?
+# What about a known email address?
+print len([x for x in enron_data.values() if x['salary'] != 'NaN'])
+print len([x for x in enron_data.values() if x['email_address'] != 'NaN'])
