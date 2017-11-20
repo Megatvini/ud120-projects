21b5c7080e3d0a68000988c7a05b501ffba8ddb2
Nika Dogonadze <ndogh13@freeuni.edu.ge>
2017-11-20 22:54:00 +0400

    Complete enron data outlier removal

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,8 +10,9 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
-    cleaned_data = []
+    errors = zip(ages, net_worths, map(lambda x: (x[0] - x[1])**2, zip(predictions, net_worths)))
+    cleaned_data = sorted(errors, key=lambda x: x[2])
+    cleaned_data = cleaned_data[:int(len(cleaned_data)*0.90)]
 
     ### your code goes here
 
